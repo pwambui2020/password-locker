1421315003bdc49738e42558f77ff47a57130b54
Paulyne <paulynewambui2020@gmail.com>
2020-10-11 00:00:17 +0300

    add the new navigate method which holds the option inputs

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # This is an application 
 
 #  class main:
+
+def navigate (x):
+    if x=="1":
+      main.Register()
+    elif x=="2":
+        print("Login")
+    elif x=="3":
+        exit()
+    else:
+        print ("invalid password")
+
+
+    
 print ("Welcome to Password Locker")
 print ("Select an option to continue")
 print ("1.Register\n2.Login\n3.Exit") 
