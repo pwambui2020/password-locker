f7d71fb85c829e686cb04d44697edf1706f6f4d7
Paulyne <paulynewambui2020@gmail.com>
2020-10-11 08:28:55 +0300

    add the register method

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # This is an application 
 
-class main:
+class Main:
+    def register():
+        print("Create new account")
+        username = input ("Enter username: ")
+        password = input (" Enter new password: ")
+        c_password = input (" Confirm your password: ")
 
-def navigate (x):
-    if x=="1":
-      main.Register()
-    elif x=="2":
-        print("Login")
-    elif x=="3":
-        exit()
-    else:
-        print ("invalid password")
+
+    def navigate (x):
+        if x=="1":
+            Main.Register()
+        elif x=="2":
+            print("Login")
+        elif x=="3":
+            exit()
+        else:
+            print ("invalid password")
 
 
     
