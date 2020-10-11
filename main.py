d12ef5c052b58c2b86152c4cd2bd1fe7b4fd0564
Paulyne <paulynewambui2020@gmail.com>
2020-10-11 22:11:50 +0300

    add the credentials class

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ class User:
     def __init__(self, username, password):
         self.username=username
         self.password=password
+        
+class credentials: 
+    def __init__(self, site ,username, password):
+        self.site=site
+        self.username=username
+        self.password=password
 
 class Main:
+    
     def register():
         print("Create new account")
         username=input("Enter username: ")
@@ -14,10 +21,23 @@ class Main:
         if password==c_password:
             user=User(username,password)
             users.append(user)
+            Main.login()
 
         else:
             print("passwords do not match")
             Main.register() 
+            
+    def login():
+        print("Login to your account")
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        for x in users:
+            if x.username==username and x.password==password:
+                print("Login successful")
+            else:
+                print("Invalid login attempt")
+                Main.login()
+
             
 
     def navigate (x):
