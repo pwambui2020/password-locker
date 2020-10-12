50d9b0b73377af52904ae76ef00c3b205aadc636
Paulyne <paulynewambui2020@gmail.com>
2020-10-12 08:01:18 +0300

    add the new credential option, existing option, viewing and delete options

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
+import random 
 # This is an application
 users=[] 
+credentials=[]
+
 class User:
     def __init__(self, username, password):
         self.username=username
         self.password=password
-        
-class credentials: 
+
+class Credentials: 
     def __init__(self, site ,username, password):
         self.site=site
         self.username=username
         self.password=password
+    def new():
+        print("Select an option to continue")
+        print("1.New credentials\n2.Existing credentials\n3.View credentials\n4.Delete credentials")
+        option=input()
+        if option=="1":
+            site=input("Enter site name: ")
+            username=input("Enter username: ")
+            print("Do you want the application to generate you a password?\n1.Yes\n2.No")
+            selection=input()
+            if selection=="1":
+                password=str(random.randint(1000000,9999999))
+            elif selection=="2":
+                password=input("Enter password: ")
+            else:
+                print("Invalid option")
+            credential=Credentials(site, username, password)
+            credentials.append(credential)
+            print("Added successfully")
+            Credentials.new()
+        elif option=="2":
+            site=input("Enter site name: ")
+            username=input("Enter username: ")
+            password=input("Enter password: ")
+            credential=Credentials(site, username, password)
+            credentials.append(credential)
+            print("Added successfully")
+            Credentials.new()
+        elif option=="3":
+            for x in credentials:
+                print("Site name: " + x.site)
+                print("username: " + x.username)
+                print("password: " + x.password)
+            Credentials.new()
+        elif option=="4":
+            print("Enter the name of the site you want to delete: ")
+            name=input()            
+            for i, o in enumerate(credentials):
+                if o.site== name:
+                    del credentials[i]
+            Credentials.new()
+        else:
+            print("Invalid option")
+
 
 class Main:
     
@@ -33,10 +79,11 @@ class Main:
         password = input("Enter your password: ")
         for x in users:
             if x.username==username and x.password==password:
-                print("Login successful")
+                Credentials.new()
             else:
                 print("Invalid login attempt")
                 Main.login()
+
 
             
 
